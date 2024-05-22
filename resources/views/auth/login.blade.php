94b5c5e9a0fa6ce1f6b6e6545e2e4044899865e5
RendyAFS <arendy724@gmail.com>
2024-05-22 10:58:24 +0700

    fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,8 +8,7 @@
             <div class="d-flex justify-content-center">
                 <div class="rounded-start-5 bg-white" style="width: 28%">
                     <div class="d-flex justify-content-center mt-5">
-                        <img class="img" style="width: 30%" src="{{ asset('assets/logo-uns.webp') }}"
-                            alt="image">
+                        <img class="img" style="width: 30%" src="{{ asset('assets/logo-uns.webp') }}" alt="image">
                     </div>
                     <div class="text">
                         <p class="h2 text-center fw-bold mt-4 mb-4">
@@ -17,9 +16,9 @@
                         </p>
                     </div>
                     <div class="input mt-5">
-                        <div class="mb-4  px-5">
+                        <div class="mb-4 px-5">
                             <label for="email" class="form-label fw-bold">Email address</label>
-                            <input type="email" class="form-control  @error('email') is-invalid @enderror" id="email"
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" id="email"
                                 name="email" placeholder="email@example.com" value="{{ old('email') }}" required
                                 autocomplete="email" autofocus>
                         </div>
@@ -32,7 +31,7 @@
                         <div class="mb-3 px-5 position-relative">
                             <label for="password" class="form-label fw-bold">Password</label>
                             <div class="input-group">
-                                <input type="password" class="form-control  @error('password') is-invalid @enderror"
+                                <input type="password" class="form-control @error('password') is-invalid @enderror"
                                     id="password" name="password" placeholder="password" required
                                     autocomplete="current-password">
                                 <div class="input-group-append">
@@ -49,15 +48,14 @@
                         @enderror
                     </div>
                     {{-- Button --}}
-                    <div class="px-5 d-flex justify-content-center ">
+                    <div class="px-5 d-flex justify-content-center">
                         <button type="submit" class="btn btn-login w-100">
                             Sign In
                         </button>
                     </div>
                 </div>
                 <div class="rounded-end-5" style="width: 28%">
-                    <img class="img" style="width: 100%"src="{{ asset('assets/foto-login.webp') }}"
-                        alt="image">
+                    <img class="img" style="width: 100%" src="{{ asset('assets/foto-login.webp') }}" alt="image">
                 </div>
             </div>
         </form>
@@ -70,8 +68,7 @@
             <div class="d-flex justify-content-center mt-3 pt-5">
                 <div class="rounded-5 bg-white" style="width: 90%">
                     <div class="d-flex justify-content-center mt-5">
-                        <img class="img" style="width: 30%" src="{{ asset('assets/logo-uns.webp') }}"
-                            alt="image">
+                        <img class="img" style="width: 30%" src="{{ asset('assets/logo-uns.webp') }}" alt="image">
                     </div>
                     <div class="text">
                         <p class="h2 text-center fw-bold mt-4 mb-4">
@@ -79,11 +76,11 @@
                         </p>
                     </div>
                     <div class="input mt-5">
-                        <div class="mb-4  px-5">
+                        <div class="mb-4 px-5">
                             <label for="email" class="form-label fw-bold">Email address</label>
-                            <input type="email" class="form-control  @error('email') is-invalid @enderror" id="email"
-                                name="email" placeholder="email@example.com" value="{{ old('email') }}" required
-                                autocomplete="email" autofocus>
+                            <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                id="email-mobile" name="email" placeholder="email@example.com" value="{{ old('email') }}"
+                                required autocomplete="email" autofocus>
                         </div>
                         @error('email')
                             <span class="invalid-feedback" role="alert">
@@ -94,11 +91,11 @@
                         <div class="mb-3 px-5 position-relative">
                             <label for="password" class="form-label fw-bold">Password</label>
                             <div class="input-group">
-                                <input type="password" class="form-control  @error('password') is-invalid @enderror"
-                                    id="password" name="password" placeholder="password" required
+                                <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                    id="password-mobile" name="password" placeholder="password" required
                                     autocomplete="current-password">
                                 <div class="input-group-append">
-                                    <span class="input-group-text" id="togglePassword" style="cursor: pointer;">
+                                    <span class="input-group-text" id="togglePassword-mobile" style="cursor: pointer;">
                                         <i class="bi bi-eye-slash"></i>
                                     </span>
                                 </div>
@@ -122,6 +119,7 @@
     </div>
 
     <script>
+        // pc
         const togglePassword = document.querySelector('#togglePassword');
         const password = document.querySelector('#password');
         const icon = togglePassword.querySelector('i');
@@ -133,6 +131,20 @@
             // toggle the eye / eye slash icon
             icon.classList.toggle('bi-eye');
             icon.classList.toggle('bi-eye-slash');
+        });
+
+        // hp
+        const togglePasswordMobile = document.querySelector('#togglePassword-mobile');
+        const passwordMobile = document.querySelector('#password-mobile');
+        const iconMobile = togglePasswordMobile.querySelector('i');
+
+        togglePasswordMobile.addEventListener('click', function(e) {
+            // toggle the type attribute
+            const type = passwordMobile.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordMobile.setAttribute('type', type);
+            // toggle the eye / eye slash icon
+            iconMobile.classList.toggle('bi-eye');
+            iconMobile.classList.toggle('bi-eye-slash');
         });
     </script>
 @endsection
